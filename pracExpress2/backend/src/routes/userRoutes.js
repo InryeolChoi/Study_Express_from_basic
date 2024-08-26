f49303b868d933462688d957cfd7a1e6ab264091
InryeolChoi <dlsfuf0316@gmail.com>
2024-08-26 22:34:25 +0900

    delete pracExpress2

--- pracExpress2/backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUserById, updateUser } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.get('/:id', getUserById);
-router.put('/:id', updateUser);
-
-export default router;
